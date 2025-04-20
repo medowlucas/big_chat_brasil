8528d1103993752b0da2016ee7c77447c578e04b
Lucas Lopes De Souza Guilha <82731221+medowlucas@users.noreply.github.com>
2025-04-20 18:49:05 -0300

    feat: adds swagger auth post

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Realiza login com CPF ou CNPJ' })
+  @ApiBody({
+    description: 'Dados necessários para login',
+    schema: {
+      type: 'object',
+      properties: {
+        documentId: { type: 'string', example: '12345678901' },
+        documentType: { type: 'string', enum: ['CPF', 'CNPJ'], example: 'CPF' },
+      },
+    },
+  })
   async login(@Body() body: { documentId: string; documentType: 'CPF' | 'CNPJ' }) {
     const { documentId, documentType } = body;
     return this.authService.login(documentId, documentType);
